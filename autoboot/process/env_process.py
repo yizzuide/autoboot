9affff70c5cfc3b0d158dab333dc5a3098f370c9
yizzuide <fu837014586@163.com>
2023-11-29 00:03:44 +0800

    fix(env): Fixed find .env file path

--- autoboot/process/env_process.py
+++ autoboot/process/env_process.py
@@ -1,17 +1,16 @@
 
 import os
-from dotenv import find_dotenv, dotenv_values
+from dotenv import dotenv_values
 
 def load_env_file(dir: str, env_name: str):
   """load env file with input opt arg: -e <name>"""
   
   _load_env_file(f"{dir}/.env")
   if env_name:
-    _load_env_file(find_dotenv(f"{dir}/.env.{env_name}"))
-      
-      
+    _load_env_file(f"{dir}/.env.{env_name}")
+
 def _load_env_file(filepath: str):
-  env_kvs = dotenv_values(find_dotenv(filepath))
+  env_kvs = dotenv_values(filepath)
   for k, v in env_kvs.items():
     os.environ[k] = v
       
