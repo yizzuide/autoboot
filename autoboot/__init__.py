b4ab056fba3d0bf7918901eafe76ea4ef33b371d
yizzuide <fu837014586@163.com>
2023-12-13 18:11:19 +0800

    chore(build): update build system

--- autoboot/__init__.py
+++ autoboot/__init__.py
@@ -2,4 +2,6 @@ from .application_properties import ApplicationProperties
 from .server_properties import ServerProperties
 from .applications import AutoBoot, AutoBootConfig
 
+VERSION  = "0.7.0"
+
 __all__ = ["AutoBoot", "AutoBootConfig", "ApplicationProperties", "ServerProperties"]
